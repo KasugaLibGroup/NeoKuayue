bc33c844c73fb229b4178c5095009c1f33e39ff3
MegumiKasuga <1926195023@qq.com>
2024-08-02 17:30:03 +0800

    实装部分刷绿25方块

--- src/main/java/willow/train/kuayue/initial/panel/C25Panel.java
+++ src/main/java/willow/train/kuayue/initial/panel/C25Panel.java
@@ -1,6 +1,113 @@
 package willow.train.kuayue.initial.panel;
 
+import net.minecraft.world.level.material.Material;
+import net.minecraft.world.level.material.MaterialColor;
+import net.minecraft.world.phys.Vec2;
+import willow.train.kuayue.block.panels.TrainPanelBlock;
+import willow.train.kuayue.block.panels.slab.HingeSlabBlock;
+import willow.train.kuayue.block.panels.slab.TrainSlabBlock;
+import willow.train.kuayue.block.panels.window.TrainSmallWindowBlock;
+import willow.train.kuayue.block.panels.window.TrainUnOpenableSmallWindowBlock;
+import willow.train.kuayue.initial.AllElements;
+import willow.train.kuayue.initial.registration.PanelRegistration;
+import willow.train.kuayue.initial.registration.SlabRegistration;
+
 public class C25Panel {
+
+    public static final PanelRegistration<TrainPanelBlock> PANEL_BOTTOM_LINE_MARSHALLED_25 =
+            new PanelRegistration<TrainPanelBlock>("panel_bottom_line_marshalled_25")
+                    .block(p -> new TrainPanelBlock(p, new Vec2(0, 0), new Vec2(1, 1)))
+                    .materialAndColor(Material.METAL, MaterialColor.COLOR_GREEN)
+                    .tab(AllElements.neoKuayueMainTab)
+                    .noOcclusion()
+                    .submit(AllElements.testRegistry);
+    public static final PanelRegistration<TrainPanelBlock> PANEL_BOTTOM_MARSHALLED_25 =
+            new PanelRegistration<TrainPanelBlock>("panel_bottom_marshalled_25")
+                    .block(p -> new TrainPanelBlock(p, new Vec2(0, 0), new Vec2(1, 1)))
+                    .materialAndColor(Material.METAL, MaterialColor.COLOR_GREEN)
+                    .tab(AllElements.neoKuayueMainTab)
+                    .noOcclusion()
+                    .submit(AllElements.testRegistry);
+    public static final PanelRegistration<TrainPanelBlock> PANEL_MIDDLE_MARSHALLED_25 =
+            new PanelRegistration<TrainPanelBlock>("panel_middle_marshalled_25")
+                    .block(p -> new TrainPanelBlock(p, new Vec2(0, 0), new Vec2(1, 1)))
+                    .materialAndColor(Material.METAL, MaterialColor.COLOR_GREEN)
+                    .tab(AllElements.neoKuayueMainTab)
+                    .noOcclusion()
+                    .submit(AllElements.testRegistry);
+    public static final PanelRegistration<TrainPanelBlock> PANEL_TOP_MARSHALLED_25 =
+            new PanelRegistration<TrainPanelBlock>("panel_top_marshalled_25")
+                    .block(p -> new TrainPanelBlock(p, new Vec2(0, 0), new Vec2(1, 1)))
+                    .materialAndColor(Material.METAL, MaterialColor.COLOR_GREEN)
+                    .tab(AllElements.neoKuayueMainTab)
+                    .noOcclusion()
+                    .submit(AllElements.testRegistry);
+    public static final PanelRegistration<TrainPanelBlock> PANEL_TOP_MARSHALLED_25_2 =
+            new PanelRegistration<TrainPanelBlock>("panel_top_marshalled_25_2")
+                    .block(p -> new TrainPanelBlock(p, new Vec2(0, 0), new Vec2(1, 1)))
+                    .materialAndColor(Material.METAL, MaterialColor.COLOR_GREEN)
+                    .tab(AllElements.neoKuayueMainTab)
+                    .noOcclusion()
+                    .submit(AllElements.testRegistry);
+
+    public static final PanelRegistration<TrainSmallWindowBlock> WINDOW_OC_25 =
+            new PanelRegistration<TrainSmallWindowBlock>("window_oc_25")
+                    .block(TrainSmallWindowBlock::new)
+                    .materialAndColor(Material.METAL, MaterialColor.COLOR_GREEN)
+                    .tab(AllElements.neoKuayueMainTab)
+                    .noOcclusion()
+                    .submit(AllElements.testRegistry);
+    public static final PanelRegistration<TrainUnOpenableSmallWindowBlock> WINDOW_OC_SEALED_25 =
+            new PanelRegistration<TrainUnOpenableSmallWindowBlock>("window_oc_sealed_25")
+                    .block(TrainUnOpenableSmallWindowBlock::new)
+                    .materialAndColor(Material.METAL, MaterialColor.COLOR_GREEN)
+                    .tab(AllElements.neoKuayueMainTab)
+                    .noOcclusion()
+                    .submit(AllElements.testRegistry);
+
+    public static final SlabRegistration<TrainSlabBlock> CARPORT_25BGZK =
+            new SlabRegistration<TrainSlabBlock>("carport_25bgzk")
+                    .block(p -> new TrainSlabBlock(p, true))
+                    .materialAndColor(Material.METAL, MaterialColor.COLOR_GREEN)
+                    .tab(AllElements.neoKuayueMainTab)
+                    .noOcclusion()
+                    .submit(AllElements.testRegistry);
+    public static final SlabRegistration<TrainSlabBlock> CARPORT_AIR_CONDITION_25BGZK =
+            new SlabRegistration<TrainSlabBlock>("carport_air_condition_25bgzk")
+                    .block(p -> new TrainSlabBlock(p, true))
+                    .materialAndColor(Material.METAL, MaterialColor.COLOR_GREEN)
+                    .tab(AllElements.neoKuayueMainTab)
+                    .noOcclusion()
+                    .submit(AllElements.testRegistry);
+    public static final SlabRegistration<HingeSlabBlock> CARPORT_AIR_CONDITION_SIDE_25BGZK =
+            new SlabRegistration<HingeSlabBlock>("carport_air_condition_side_25bgzk")
+                    .block(p -> new HingeSlabBlock(p, true))
+                    .materialAndColor(Material.METAL, MaterialColor.COLOR_GREEN)
+                    .tab(AllElements.neoKuayueMainTab)
+                    .noOcclusion()
+                    .submit(AllElements.testRegistry);
+    public static final SlabRegistration<TrainSlabBlock> CARPORT_CENTER_25BGZKT =
+            new SlabRegistration<TrainSlabBlock>("carport_center_25bgzkt")
+                    .block(p -> new TrainSlabBlock(p, true))
+                    .materialAndColor(Material.METAL, MaterialColor.COLOR_GREEN)
+                    .tab(AllElements.neoKuayueMainTab)
+                    .noOcclusion()
+                    .submit(AllElements.testRegistry);
+    public static final SlabRegistration<TrainSlabBlock> CARPORT_WATER_TANK_25BGZKT =
+            new SlabRegistration<TrainSlabBlock>("carport_water_tank_25bgzkt")
+                    .block(p -> new TrainSlabBlock(p, true))
+                    .materialAndColor(Material.METAL, MaterialColor.COLOR_GREEN)
+                    .tab(AllElements.neoKuayueMainTab)
+                    .noOcclusion()
+                    .submit(AllElements.testRegistry);
+
+    public static final SlabRegistration<TrainSlabBlock> FLOOR_MARSHALLED_25 =
+            new SlabRegistration<TrainSlabBlock>("floor_marshalled_25")
+                    .block(p -> new TrainSlabBlock(p, false))
+                    .materialAndColor(Material.METAL, MaterialColor.COLOR_GREEN)
+                    .tab(AllElements.neoKuayueMainTab)
+                    .noOcclusion()
+                    .submit(AllElements.testRegistry);
 
     public static void invoke(){}
 }
