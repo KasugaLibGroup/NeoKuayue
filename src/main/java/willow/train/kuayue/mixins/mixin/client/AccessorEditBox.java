a89e3f0261016dff4b99ed48cbc21c7ca1ca612f
MegumiKasuga <1926195023@qq.com>
2024-09-10 12:30:25 +0800

    尝试修复EditBoxMixin问题

--- src/main/java/willow/train/kuayue/mixins/mixin/client/AccessorEditBox.java
+++ src/main/java/willow/train/kuayue/mixins/mixin/client/AccessorEditBox.java
@@ -3,9 +3,6 @@ package willow.train.kuayue.mixins.mixin.client;
 import net.minecraft.client.gui.Font;
 import net.minecraft.client.gui.components.EditBox;
 import net.minecraft.util.FormattedCharSequence;
-import net.minecraftforge.api.distmarker.Dist;
-import net.minecraftforge.api.distmarker.OnlyIn;
-import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.gen.Accessor;
 
