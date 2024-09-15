287662dab093ff7b793cf87d3226c1d8368a0a00
zlw380 <496607959@qq.com>
2024-09-15 14:54:48 +0800

    DF21机车设备间窗户 油箱 通用下厢板

--- src/main/java/willow/train/kuayue/initial/panel/I21Panel.java
+++ src/main/java/willow/train/kuayue/initial/panel/I21Panel.java
@@ -8,13 +8,16 @@ import net.minecraft.world.level.block.SlabBlock;
 import net.minecraft.world.level.block.state.BlockBehaviour;
 import net.minecraft.world.level.material.Material;
 import net.minecraft.world.level.material.MaterialColor;
+import net.minecraft.world.phys.Vec2;
 import net.minecraft.world.phys.Vec3;
 import willow.train.kuayue.block.panels.FullShapeDirectionalBlock;
+import willow.train.kuayue.block.panels.TrainHingePanelBlock;
 import willow.train.kuayue.block.panels.door.CustomRenderedDoorBlock;
 import willow.train.kuayue.block.panels.slab.CarportHingeSlabBlock;
 import willow.train.kuayue.block.panels.slab.HeightSlabBlock;
 import willow.train.kuayue.block.panels.slab.HingeSlabBlock;
 import willow.train.kuayue.block.panels.slab.TrainSlabBlock;
+import willow.train.kuayue.block.panels.window.TrainUnOpenableWindowBlock;
 import willow.train.kuayue.initial.AllElements;
 import willow.train.kuayue.initial.registration.PanelRegistration;
 import willow.train.kuayue.initial.registration.SlabRegistration;
@@ -92,6 +95,31 @@ public class I21Panel {
                     .materialAndColor(Material.METAL, MaterialColor.COLOR_GREEN)
                     .noOcclusion()
                     .tab(AllElements.neoKuayueLocoTab)
+                    .submit(AllElements.testRegistry);
+
+    public static final PanelRegistration<TrainUnOpenableWindowBlock> WINDOW_EQUIP_DF21 =
+            new PanelRegistration<TrainUnOpenableWindowBlock>("window_equip_df21")
+                    .block(p -> new TrainUnOpenableWindowBlock(p, 3))
+                    .materialAndColor(Material.METAL, MaterialColor.COLOR_GREEN)
+                    .tab(AllElements.neoKuayueLocoTab)
+                    .noOcclusion()
+                    .submit(AllElements.testRegistry);
+
+    public static final PanelRegistration<TrainHingePanelBlock> GENERAL_BOTTOM_SLAB_DF21 =
+            new PanelRegistration<TrainHingePanelBlock>("general_bottom_slab_df21")
+                    .block(p -> new TrainHingePanelBlock(p, new Vec2(0, 0), new Vec2(1, 1)))
+                    .materialAndColor(Material.METAL, MaterialColor.COLOR_GREEN)
+                    .tab(AllElements.neoKuayueLocoTab)
+                    .noOcclusion()
+                    .submit(AllElements.testRegistry);
+
+    public static final BlockReg<FullShapeDirectionalBlock> FUEL_TANK_DF21 =
+            new BlockReg<FullShapeDirectionalBlock>("fuel_tank_df21")
+                    .blockType(FullShapeDirectionalBlock::new)
+                    .material(Material.METAL).materialColor(MaterialColor.COLOR_BLACK)
+                    .addProperty(BlockBehaviour.Properties::noOcclusion)
+                    .defaultBlockItem()
+                    .tabTo(AllElements.neoKuayueLocoTab)
                     .submit(AllElements.testRegistry);
 
     public static void invoke(){}
