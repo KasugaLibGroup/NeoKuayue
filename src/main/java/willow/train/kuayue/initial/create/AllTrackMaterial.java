99d72818f28bb7bd9bd7dab3f1edd37c9d38f28a
MegumiKasuga <1926195023@qq.com>
2024-11-01 16:31:06 +0800

    - 适应性修改，将铁轨物品重新加入tab。 - tab 中物品目前将不再混乱。

--- src/main/java/willow/train/kuayue/initial/create/AllTrackMaterial.java
+++ src/main/java/willow/train/kuayue/initial/create/AllTrackMaterial.java
@@ -10,8 +10,6 @@ import willow.train.kuayue.initial.AllElements;
 
 public class AllTrackMaterial {
 
-    public static final CreateRegistry testRegistry = AllElements.testRegistry;
-
     public static final SimpleTrackBlock.Builder builder =
             new SimpleTrackBlock.Builder(() -> AllCarriageBogeys.carriageBlockBundle.getElement("pk209p_bogey").getEntry().get());
 
@@ -22,57 +20,57 @@ public class AllTrackMaterial {
             .lang("standard_track")
             .block(() -> AllTracks.standardTrack)
             .trackParticle(new ResourceLocation("minecraft", "block/palettes/stone_types/polished/andesite_cut_polished"))
-            .type(testRegistry.asResource("standard"), builder::build)
+            .type(AllElements.testRegistry.asResource("standard"), builder::build)
             .customModel(
-                    () -> new PartialModel(testRegistry.asResource("block/track/standard/tie")),
-                    () -> new PartialModel(testRegistry.asResource("block/track/standard/segment_left")),
-                    () -> new PartialModel(testRegistry.asResource("block/track/standard/segment_right"))
+                    () -> new PartialModel(AllElements.testRegistry.asResource("block/track/standard/tie")),
+                    () -> new PartialModel(AllElements.testRegistry.asResource("block/track/standard/segment_left")),
+                    () -> new PartialModel(AllElements.testRegistry.asResource("block/track/standard/segment_right"))
             )
             .simpleTrackModelOffset(0.755f)
             .sleeper(Blocks.ANDESITE_SLAB)
-            .submit(testRegistry);
+            .submit(AllElements.testRegistry);
 
     public static final TrackMaterialReg tielessMaterial = new TrackMaterialReg("tieless")
             .lang("tieless_track")
             .block(() -> AllTracks.tielessTrack)
             .trackParticle(new ResourceLocation("minecraft", "block/palettes/stone_types/polished/andesite_cut_polished"))
-            .type(testRegistry.asResource("tieless"), builder::build)
+            .type(AllElements.testRegistry.asResource("tieless"), builder::build)
             .customModel(
                     () -> new PartialModel(new ResourceLocation("kasuga_lib", "empty_model")),
-                    () -> new PartialModel(testRegistry.asResource("block/track/standard/segment_left")),
-                    () -> new PartialModel(testRegistry.asResource("block/track/standard/segment_right"))
+                    () -> new PartialModel(AllElements.testRegistry.asResource("block/track/standard/segment_left")),
+                    () -> new PartialModel(AllElements.testRegistry.asResource("block/track/standard/segment_right"))
             )
             .simpleTrackModelOffset(0.755f)
             .sleeper(Blocks.ANDESITE_SLAB)
-            .submit(testRegistry);
+            .submit(AllElements.testRegistry);
 
     public static final TrackMaterialReg ballastlessMaterial = new TrackMaterialReg("ballastless")
             .lang("ballastless_track")
             .block(() -> AllTracks.ballastlessTrack)
             .trackParticle(new ResourceLocation("minecraft", "block/palettes/stone_types/polished/andesite_cut_polished"))
-            .type(testRegistry.asResource("ballastless"), builder::build)
+            .type(AllElements.testRegistry.asResource("ballastless"), builder::build)
             .customModel(
-                    () -> new PartialModel(testRegistry.asResource("block/track/ballastless/tie")),
-                    () -> new PartialModel(testRegistry.asResource("block/track/standard/segment_left")),
-                    () -> new PartialModel(testRegistry.asResource("block/track/standard/segment_right"))
+                    () -> new PartialModel(AllElements.testRegistry.asResource("block/track/ballastless/tie")),
+                    () -> new PartialModel(AllElements.testRegistry.asResource("block/track/standard/segment_left")),
+                    () -> new PartialModel(AllElements.testRegistry.asResource("block/track/standard/segment_right"))
             )
             .simpleTrackModelOffset(0.755f)
             .sleeper(Blocks.ANDESITE_SLAB)
-            .submit(testRegistry);
+            .submit(AllElements.testRegistry);
 
     public static final TrackMaterialReg meterMaterial = new TrackMaterialReg("meter")
             .lang("meter_track")
             .block(() -> AllTracks.meterTrack)
             .trackParticle(new ResourceLocation("minecraft", "block/palettes/stone_types/polished/andesite_cut_polished"))
-            .type(testRegistry.asResource("meter"), meterBuilder::build)
+            .type(AllElements.testRegistry.asResource("meter"), meterBuilder::build)
             .customModel(
-                    () -> new PartialModel(testRegistry.asResource("block/track/meter/tie")),
-                    () -> new PartialModel(testRegistry.asResource("block/track/standard/segment_left")),
-                    () -> new PartialModel(testRegistry.asResource("block/track/standard/segment_right"))
+                    () -> new PartialModel(AllElements.testRegistry.asResource("block/track/meter/tie")),
+                    () -> new PartialModel(AllElements.testRegistry.asResource("block/track/standard/segment_left")),
+                    () -> new PartialModel(AllElements.testRegistry.asResource("block/track/standard/segment_right"))
             )
             .simpleTrackModelOffset(0.525f)
             .sleeper(Blocks.ANDESITE_SLAB)
-            .submit(testRegistry);
+            .submit(AllElements.testRegistry);
 
     public static void invoke(){}
 }
