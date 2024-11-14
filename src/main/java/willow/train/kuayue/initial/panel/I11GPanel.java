845be5aac26048893adb9c9e3c9591ac305361c8
aurora4123 <1652039138@qq.com>
2024-11-15 02:07:34 +0800

    df11g上厢板

--- src/main/java/willow/train/kuayue/initial/panel/I11GPanel.java
+++ src/main/java/willow/train/kuayue/initial/panel/I11GPanel.java
@@ -203,4 +203,11 @@ public class I11GPanel {
                     .submit(AllElements.testRegistry);
 
     public static void invoke(){}
+    public static final PanelRegistration<TrainPanelBlock> PANEL_TOP_DF11G =
+            new PanelRegistration<TrainPanelBlock>("panel_top_df11g")
+                    .block(p -> new TrainPanelBlock(p, new Vec2(0,0),new Vec2(1,1)))
+                    .materialAndColor(Material.METAL,MaterialColor.COLOR_BLUE)
+                    .tab(AllElements.neoKuayueLocoTab)
+                    .noOcclusion().strengthAndTool(1.5f,3f)
+                    .submit(AllElements.testRegistry);
 }
