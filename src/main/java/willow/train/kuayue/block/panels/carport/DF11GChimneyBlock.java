ad456ab682797023ca07aa42de8b203c13984779
zlw380 <496607959@qq.com>
2024-12-01 11:27:05 +0800

    DF11G烟囱

---
+++ src/main/java/willow/train/kuayue/block/panels/carport/DF11GChimneyBlock.java
@@ -0,0 +1,96 @@
+package willow.train.kuayue.block.panels.carport;
+
+import net.minecraft.core.BlockPos;
+import net.minecraft.core.Direction;
+import net.minecraft.core.particles.ParticleTypes;
+import net.minecraft.util.RandomSource;
+import net.minecraft.world.InteractionResult;
+import net.minecraft.world.item.context.BlockPlaceContext;
+import net.minecraft.world.item.context.UseOnContext;
+import net.minecraft.world.level.Level;
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.level.block.state.StateDefinition;
+import net.minecraft.world.level.block.state.properties.BlockStateProperties;
+import net.minecraft.world.level.block.state.properties.BooleanProperty;
+import org.jetbrains.annotations.Nullable;
+import willow.train.kuayue.block.panels.slab.HingeSlabBlock;
+
+public class DF11GChimneyBlock extends HingeSlabBlock {
+
+    public static final BooleanProperty LIT = BlockStateProperties.LIT;
+
+    public DF11GChimneyBlock(Properties properties, boolean isCarport) {
+        super(properties, isCarport);
+        registerDefaultState(this.getStateDefinition().any()
+                .setValue(LIT, true));
+    }
+
+    @Override
+    protected void createBlockStateDefinition(StateDefinition.Builder<Block, BlockState> pBuilder) {
+        super.createBlockStateDefinition(pBuilder.add(LIT));
+    }
+
+    @Override
+    public @Nullable BlockState getStateForPlacement(BlockPlaceContext pContext) {
+        return super.getStateForPlacement(pContext)
+                .setValue(LIT, true);
+    }
+
+    @Override
+    public InteractionResult onWrenched(BlockState state, UseOnContext context) {
+        Level level = context.getLevel();
+        BlockPos pos = context.getClickedPos();
+        if(!level.isClientSide()) {
+            level.setBlock(pos,state.cycle(LIT),3);
+        }
+        return InteractionResult.SUCCESS;
+    }
+
+    @Override
+    public InteractionResult onSneakWrenched(BlockState state, UseOnContext context) {
+        Level level = context.getLevel();
+        BlockPos pos = context.getClickedPos();
+        if(!level.isClientSide()) {
+            level.setBlock(pos,state.cycle(BlockStateProperties.DOOR_HINGE),3);
+        }
+        return InteractionResult.SUCCESS;
+    }
+
+    @Override
+    public void animateTick(BlockState pState, Level pLevel, BlockPos pPos, RandomSource pRandom) {
+        spawnParticles(pState, pLevel, pPos);
+    }
+
+    private static void spawnParticles(BlockState pState, Level pLevel, BlockPos pPos) {
+        RandomSource pRandom = pLevel.random;
+        Direction direction = pState.getValue(FACING);
+
+        if (pState.getValue(LIT)) {
+            for (int i = 0; i < 5; i++) {
+                if (pRandom.nextFloat() < 0.5f) {
+                    pLevel.addParticle(
+                            ParticleTypes.LARGE_SMOKE,
+                            (double)pPos.getX() + 0.5D,
+                            (double)pPos.getY() + 1.0D,
+                            (double)pPos.getZ() + 0.5D,
+                            (direction == Direction.EAST || direction == Direction.WEST) ?
+                                    (double)(0.05F + pRandom.nextFloat() / 10.0F) : 0.0F,
+                            0.2F,
+                            (direction == Direction.SOUTH || direction == Direction.NORTH) ?
+                                    (double)(0.05F + pRandom.nextFloat() / 10.0F) : 0.0F);
+                }
+                pLevel.addParticle(
+                        ParticleTypes.LARGE_SMOKE,
+                        (double)pPos.getX() + 0.5D,
+                        (double)pPos.getY() + 1.0D,
+                        (double)pPos.getZ() + 0.5D,
+                        (direction == Direction.EAST || direction == Direction.WEST) ?
+                                -(double)(0.05F + pRandom.nextFloat() / 10.0F) : 0.0F,
+                        0.2F,
+                        (direction == Direction.SOUTH || direction == Direction.NORTH) ?
+                                -(double)(0.05F + pRandom.nextFloat() / 10.0F) : 0.0F);
+            }
+        }
+    }
+}
--- src/main/java/willow/train/kuayue/initial/panel/I11GPanel.java
+++ src/main/java/willow/train/kuayue/initial/panel/I11GPanel.java
@@ -12,6 +12,7 @@ import willow.train.kuayue.block.panels.FullShapeDirectionalBlock;
 import willow.train.kuayue.block.panels.TrainHingePanelBlock;
 import willow.train.kuayue.block.panels.TrainPanelBlock;
 import willow.train.kuayue.block.panels.base.TrainPanelProperties;
+import willow.train.kuayue.block.panels.carport.DF11GChimneyBlock;
 import willow.train.kuayue.block.panels.door.CustomRenderedDoorBlock;
 import willow.train.kuayue.block.panels.end_face.CustomRenderedEndfaceBlock;
 import willow.train.kuayue.block.panels.slab.HingeSlabBlock;
@@ -435,6 +436,14 @@ public class I11GPanel {
                     .materialAndColor(Material.METAL,MaterialColor.COLOR_BLUE)
                     .tab(AllElements.neoKuayueLocoTab)
                     .noOcclusion().strengthAndTool(1.5f,3f)
+                    .submit(AllElements.testRegistry);
+
+    public static final SlabRegistration<DF11GChimneyBlock> DF11G_CHIMNEY =
+            new SlabRegistration<DF11GChimneyBlock>("df11g_chimney")
+                    .block(p -> new DF11GChimneyBlock(p, false))
+                    .materialAndColor(Material.METAL, MaterialColor.COLOR_GREEN)
+                    .tab(AllElements.neoKuayueLocoTab)
+                    .noOcclusion().strengthAndTool(1.5f, 3f)
                     .submit(AllElements.testRegistry);
 
     public static void invoke(){}
